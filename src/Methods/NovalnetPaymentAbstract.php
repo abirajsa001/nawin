ceb28ee53e2ed1ce56258db723087830044fcb48
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-04-09 11:36:01 +0530

    name pay

--- src/Methods/NovalnetPaymentAbstract.php
+++ src/Methods/NovalnetPaymentAbstract.php
@@ -83,13 +83,10 @@ abstract class NovalnetPaymentAbstract extends PaymentMethodBaseService
             $isPaymentActive = ($guaranteeStatus == 'normal') ? true : false;
         }
         if($isPaymentActive && $this::PAYMENT_KEY == 'NOVALNET_PAYCONIQ'){
-            $this->getLogger(__METHOD__)->error('get active' . $isPaymentActive, $this);
-            $this->getLogger(__METHOD__)->error('get name' . $this::PAYMENT_KEY, $this);
-            $isPaymentActive = false;
+
+            return false;
 
         }
-        $this->getLogger(__METHOD__)->error('test' . $isPaymentActive, $this);
-        $this->getLogger(__METHOD__)->error('test name' . $this::PAYMENT_KEY, $this);
         if($isPaymentActive && $this::PAYMENT_KEY == 'NOVALNET_SEPA') {
             $guaranteeStatus = $this->paymentService->isGuaranteePaymentToBeDisplayed($this->basketRepository, 'novalnet_guaranteed_sepa');
             $isPaymentActive = ($guaranteeStatus == 'normal') ? true : false;
