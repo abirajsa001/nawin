934e84cc91f6228957df26d86ac7806611ee52b6
abirajsa001 <abiraj_s@novalnetsolutions.com>
2026-04-10 11:53:02 +0530

    $transactionData['cycle_amount']

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -1123,7 +1123,7 @@ public function allowedCountries(Basket $basket, $allowedCountry): bool
      */
     public function getBankDetailsInformation($transactionData)
     {
-        if(in_array($transactionData['paymentName'], ['novalnet_instalment_invoice', 'novalnet_instalment_sepa'])) {
+        if(in_array($transactionData['paymentName'], ['novalnet_instalment_invoice', 'novalnet_instalment_sepa']) && $transactionData['cycle_amount']) {
             $invoiceComments = PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transfer_amount_duedate_text'), number_format($transactionData['cycle_amount'] / 100 ,2), $transactionData['currency'], date('Y/m/d', (int)strtotime($transactionData['due_date'])));
             // If the transaction is in On-Hold not displaying the due date
             if($transactionData['tx_status'] == 'ON_HOLD') {
